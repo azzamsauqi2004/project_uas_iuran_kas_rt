75c4a5cd1992c00b5f2afbe844e360b5f2a4011d
Angga674 <116695529+Angga674@users.noreply.github.com>
2024-01-05 21:16:52 +0700

    Dafa Alfiana E
    Menambahkan dml_crud

---
+++ dml_crud.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE iuran_kas_rt
+
+-- Tabel users
+
+CREATE TABLE users (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(100) NULL,
+    `password` VARCHAR(100) NULL,
+    `nama` VARCHAR(200) NULL,
+    `email` VARCHAR(200) NULL,
+    `status` TINYINT(1) NULL,
+    `role` TINYINT(1) NULL DEFAULT 2 '1:Admin\n2:User',
+    PRIMARY KEY (id)
+    UNIQUE INDEX `username_UNIQUE` (`username` ASC)
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+
+-- Tabel Warga
+CREATE TABLE warga (
+`id` INT NOT NULL AUTO_INCREMENT,
+`nik` VARCHAR(50) NULL,
+`nama` VARCHAR(200) NULL,
+`jenis_kelamin` ENUM('L', 'P') NULL,
+`no_hp` VARCHAR(20) NULL,
+`alamat` TINYTEXT NULL,
+`no_rumah` VARCHAR(10) NULL,
+`status` TINYINT(1) NULL,
+`users_id` INT NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `nik_UNIQUE` (`nik` ASC),
+INDEX `fk_warga_users1_idx` (`users_id` ASC),
+CONSTRAINT `fk_warga_users1`
+FOREIGN KEY (`users_id`)
+REFERENCES `mydb`.`users` (`id`)
+) 
+
+-- Tabel Iuran
+CREATE TABLE iuran (
+`id` INT NOT NULL AUTO_INCREMENT,
+`tanggal` DATE NULL,
+`warga_id` INT NOT NULL,
+`nominal` DECIMAL(10,2) NULL,
+`keterangan` TINYTEXT NULL,
+`jenis_iuran` TINYINT(1) NULL,
+PRIMARY KEY (`id`),
+INDEX `fk_iuran_warga1_idx` (`warga_id` ASC),
+CONSTRAINT `fk_iuran_warga1`
+FOREIGN KEY (`warga_id`)
+REFERENCES `mydb`.`warga` (`id`))
